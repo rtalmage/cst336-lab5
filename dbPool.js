3cc0347111dcf572db57f7d025732652058770a4
rtalmage <raymondtalmage@gmail.com>
2020-08-02 12:20:40 -0700

    set up jawsdb

--- dbPool.js
+++ dbPool.js
@@ -2,10 +2,10 @@ const mysql = require('mysql');
 
 const pool  = mysql.createPool({
     connectionLimit: 10,
-    host: "127.0.0.1",
-    user: "usernamehere",
-    password: "passwordhere",
-    database: "cst336-lab5"
+    host: "jawsdb",
+    user: "aijhie380lu9q927",
+    password: "ttvww8it14fd0aap",
+    database: "lltimlsvuc19oe1d"
 });
 
 module.exports = pool;
