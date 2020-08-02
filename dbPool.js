e15cd392a41a2e3393f9e00eeab3c1039e66a489
rtalmage <raymondtalmage@gmail.com>
2020-08-02 12:24:21 -0700

    set up jawsdb(fixed hostname)

--- dbPool.js
+++ dbPool.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 const pool  = mysql.createPool({
     connectionLimit: 10,
-    host: "jawsdb",
+    host: "iwqrvsv8e5fz4uni.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
     user: "aijhie380lu9q927",
     password: "ttvww8it14fd0aap",
     database: "lltimlsvuc19oe1d"
