03565a848d309dd4a5b4e30c725ab5bd8a040688
rtalmage <raymondtalmage@gmail.com>
2020-08-02 11:24:37 -0700

    commit for heroku env variables

--- app.js
+++ app.js
@@ -93,6 +93,6 @@ app.get("/api/getFavorites", function(req, res){
 
 
 //server
-app.listen("8080", "127.0.0.1", function () {
+app.listen(process.env.PORT, process.env.IP, function () {
     console.log("Express server is running...");
 });
