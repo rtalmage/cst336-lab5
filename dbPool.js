da9654a9e78e3db37748fdf0c804ad6a3f41127b
rtalmage <raymondtalmage@gmail.com>
2020-08-02 11:43:52 -0700

    put placeholders in the username and password sections of dbPool

--- dbPool.js
+++ dbPool.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const pool  = mysql.createPool({
     connectionLimit: 10,
     host: "127.0.0.1",
-    user: "root",
-    password: "starwars1",
+    user: "usernamehere",
+    password: "passwordhere",
     database: "cst336-lab5"
 });
 
